8269c24e7042bfae2bd2c6a6696ecc63d4b09847
DooMLoRD <metoo.mememe@gmail.com>
2013-04-25 10:45:47 +0530

    updated fingerprint, build description and product name
    Change-Id: I97b883acd72693d5e9cc9ad4803397b8626fd00e

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6907_1271-0336 BUILD_FINGERPRINT=Sony/C6907_1271-0336/C6907:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6907-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311_1272-1907/SGP311:4.1.2/10.1.C.0.344/p35_rw:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 4.1.2 10.1.C.0.344 p35_rw test-keys"
 
 PRODUCT_NAME := cm_pollux
 PRODUCT_DEVICE := pollux
