fa8ffbff83e9949b51d2275fc682de4c1d6aa6e3
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-05-15 20:58:58 +0200

    pollux: remove torch, vibrator and proximity
    Change-Id: Iba739eca6653d9faf9c850147be03ea173ff7d86

--- full_pollux.mk
+++ full_pollux.mk
@@ -22,10 +22,6 @@
 #
 #
  
-# Torch
-PRODUCT_PACKAGES := \
-    Torch
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
