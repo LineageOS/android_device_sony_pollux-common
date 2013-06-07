82551832fe34061d54a1c5e1325f7860d3529375
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-07 11:33:13 +0530

    relocate TARGET_SCREEN_{WIDTH,HEIGHT}
    Change-Id: I619dc81e3de223317e77d88f97f495eb4eb6e10f

--- pollux-common.mk
+++ pollux-common.mk
@@ -17,6 +17,9 @@ $(call inherit-product, device/sony/fusion3-common/fusion3.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
