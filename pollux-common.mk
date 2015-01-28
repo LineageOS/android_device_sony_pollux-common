ff7ac4215c09045603606662793d14d043ef7196
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-01-28 13:41:22 +0100

    pollux-common: move device init down one level
    Change-Id: Ia41b090daaa678239ca4b73e4194f3d164cef730

--- pollux-common.mk
+++ pollux-common.mk
@@ -50,10 +50,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/usr/idc/clearpad.idc:system/usr/idc/clearpad.idc
 
-# Device specific init
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.device.rc:root/init.device.rc
-
 # USB function switching
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc
