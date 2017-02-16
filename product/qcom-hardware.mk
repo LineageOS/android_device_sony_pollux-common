1a2b9eda6b3f0115ab05179df4655819a8eefebf
Jaekyun Seok <jaekyun@google.com>
2017-02-16 13:28:13 +0900

    vendor: Move device specific system configs into /vendor/etc
    Test: building succeeded and tested
    Bug: 35369237
    Change-Id: I9aaa226af76246d0e589049a9a8c9093f0267108

--- product/qcom-hardware.mk
+++ product/qcom-hardware.mk
@@ -1,3 +1,3 @@
 # Hardware permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+    frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
