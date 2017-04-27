ecafe5d848e243d61aeace5f16fc422b3984b607
Chippa-a <vusal1372@gmail.com>
2017-04-27 18:50:32 +0300

    pollux-common: overlay: Add freeform window management support
    Change-Id: Ic4292b2a2ba96a5b548ea13c526ff6a0640624f0

--- product/qcom-hardware.mk
+++ product/qcom-hardware.mk
@@ -1,3 +1,4 @@
 # Hardware permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
